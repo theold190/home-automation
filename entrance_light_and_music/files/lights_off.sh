f65c553a0dd7b1665f2d9e452f452e7efa157516
TheOld190 <oleg.krasnukhin@gmail.com>
2020-05-10 17:21:31 +0200

    Use Python venv for entrance setup, makes setup more like others

--- entrance_light_and_music/files/lights_off.sh
+++ entrance_light_and_music/files/lights_off.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# using full path, since script will run by cron
-/usr/local/bin/rpi-rf_send -g 27 -p 398 -t 1 83028
+set -euxo pipefail
+WORK_DIR="/home/pi/entrance"
+
+"${WORK_DIR}"/.venv/bin/rpi-rf_send -g 27 -p 398 -t 1 83028
 
